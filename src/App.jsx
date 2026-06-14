21df0881153a72b2a2856dcbae00ac9f96c987e3
Manas <manaskulkarni22@gmail.com>
2026-06-14 19:20:55 +0530

    fuk it

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-return (
-  <HashRouter>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <MainLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<DashboardPage />} />
-        <Route path="orders" element={<OrdersPage />} />
-        <Route path="customers" element={<CustomersPage />} />
-        <Route path="orders/:id" element={<OrderDetailsPage />} />
-      </Route>
-    </Routes>
-  </HashRouter>
-);
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import CustomersPage from "./pages/CustomersPage";
+import MainLayout from "./layouts/MainLayout";
+import OrdersPage from "./pages/OrdersPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<DashboardPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+          <Route path="customers" element={<CustomersPage />} />
+          <Route path="orders/:id" element={<OrderDetailsPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
